c4a5c3300e66789d29d599d376425edef07d6a74
Thomas DiModica <ricinwich@yahoo.com>
2023-04-07 22:24:05 -0600

    Ubuntu tells me that I'm depending on transitive header inclusion.

--- OddsAndEnds/LibraryLoader.cpp
+++ OddsAndEnds/LibraryLoader.cpp
@@ -31,6 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <iostream>
 #include <functional>
+#include <algorithm>
 
 #include "Backwards/Input/Lexer.h"
 #include "Backwards/Input/LineBufferedStreamInput.h"
